f447ee29235c6519d1f07cba02850e947ec0a7a9
acerockracer <90396781+acerockracer@users.noreply.github.com>
2021-09-14 10:11:21 -0500

    Add files via upload

---
+++ GitHub/GitHub/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitHub
+//
+//  Created by AMStudent on 9/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("shoot the target")
+                .padding()
+    Button(action: {}) {
+            Text("Hit me")
+    }
+    }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
